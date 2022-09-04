05c4224f8ebf9a81f66836589638c4a9f78a8146
git-nasdakr <112802597+git-nasdakr@users.noreply.github.com>
2022-09-04 22:49:21 +0900

    예전거와 비교 추가

--- web.py
+++ web.py
@@ -5,6 +5,9 @@ app = Flask(__name__) #
 @app.route('/')
 def main():
     return render_template("pricing.html")
+@app.route('/old')
+def main():
+    return render_template("pricing_old.html")
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
