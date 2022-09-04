978a4f1198a7bca31324ad668d266f2ff9a0c129
git-nasdakr <112802597+git-nasdakr@users.noreply.github.com>
2022-09-04 22:23:45 +0900

    대칭 안맞는거랑, 폰트 크기 불편한거 수정, 가격페이지 버튼 색 이상한거 수정

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #
 
 @app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template("pricing.html")
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
